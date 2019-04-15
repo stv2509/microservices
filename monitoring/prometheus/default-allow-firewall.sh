09a97f75ccc637d4567739c96492078d2d6b91d2
Dmitriy Sobolev <stv2509@gmail.com>
2019-04-15 14:08:11 +0300

    Homework 22. Prometheus. cAdvisor. Grafana. AlertManager

---
+++ monitoring/prometheus/default-allow-firewall.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+if gcloud compute firewall-rules list --format=json | grep cadvisor
+then
+	echo -e "\n #######- Firewall-rules \"default-allow-cadvisor\" alredy install -#######\n\n"
+else
+	echo -e "#######- INSTALL default-allow-cadvisor -#######\n"
+	gcloud compute firewall-rules create default-allow-cadvisor \
+ --allow tcp:8080 --priority=65534 \
+ --description="Allows cAdvisor connections" \
+ --direction=INGRESS
+	echo -e "\n#######- Installation completed -#######\n\n"
+fi
+
+
+if gcloud compute firewall-rules list --format=json | grep grafana
+then
+	echo -e "\n #######- Firewall-rules \"default-allow-grafana\" alredy install -#######\n\n"
+else
+	echo -e "#######- INSTALL default-allow-grafana -#######\n"
+	gcloud compute firewall-rules create default-allow-grafana \
+ --allow tcp:3000 --priority=65534 \
+ --description="Allows Grafana connections" \
+ --direction=INGRESS
+	echo -e "\n#######- Installation completed -#######"
+fi
+
+
+
+if gcloud compute firewall-rules list --format=json | grep alertmanager
+then
+	echo -e "\n #######- Firewall-rules \"default-allow-alertmanager\" alredy install -#######\n\n"
+else
+	echo -e "#######- INSTALL default-allow-alertmanager -#######\n"
+	gcloud compute firewall-rules create default-allow-alertmanager \
+ --allow tcp:9093 --priority=65534 \
+ --description="Allows Alertmanager connections" \
+ --direction=INGRESS
+	echo -e "\n#######- Installation completed -#######"
+fi
